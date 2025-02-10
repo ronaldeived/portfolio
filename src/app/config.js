f6b6d1f05e86397459a141e1876edbd7bc66fe9c
Ronaldeived Silva <rona_dev@outlook.com>
2025-02-09 21:56:03 -0300

    Creating the main structure

---
+++ src/app/config.js
@@ -0,0 +1,5 @@
+export const socialLinks = {
+    github: "https://github.com/ronaldeived",
+    linkedin: "https://www.linkedin.com/in/ronaldeived/",
+    email: "mailto:rona_dev@outlook.com",
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { socialLinks } from "@/app/config";
 
 export default function Home() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  );
+    return (
+        <section>
+            <a href={socialLinks.linkedin} target="_blank">
+                <Image
+                    src="/profile.png"
+                    alt="Profile photo"
+                    className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+                    unoptimized
+                    width={160}
+                    height={160}
+                    priority
+                />
+            </a>
+            <h1 className="mb-8 text-2xl font-medium tracking-tight">
+                My Portfolio!
+            </h1>
+            <div className="prose prose-neutral dark:prose-invert">
+            </div>
+        </section>
+    );
 }
