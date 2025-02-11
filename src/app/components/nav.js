cebc8b4e5639de7ea4905d09100a9e13d88aabbd
Ronaldeived Silva <rona_dev@outlook.com>
2025-02-10 21:50:14 -0300

    Some adjusts in css

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 const navItems = {
-    "/": { name: "Home" },
     "/projects": { name: "Projects" }
 };
 
@@ -9,8 +8,11 @@ export function Navbar() {
     return (
         <nav className="lg:mb-16 mb-12 py-5">
             <div className="flex flex-col md:flex-row md:items-center justify-between">
+                <Link href="/" className="text-3xl font-semibold tracking-tight">
+                    Ronaldeived
+                </Link>
                 <div className="flex flex-row gap-4 mt-6 md:mt-0 md:ml-auto items-center">
-                    {Object.entries(navItems).map(([path, { name }]) => (
+                    {Object.entries(navItems).map(([path, {name}]) => (
                         <Link
                             key={path}
                             href={path}
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
         <section>
             <a href={socialLinks.linkedin} target="_blank">
                 <Image
-                    src="/profile.png"
+                    src="/images/me.png"
                     alt="Profile photo"
                     className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
                     unoptimized
@@ -18,6 +18,9 @@ export default function Home() {
             <h1 className="mb-8 text-2xl font-medium tracking-tight">
                 My Portfolio!
             </h1>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
             <div className="prose prose-neutral dark:prose-invert">
             </div>
         </section>
